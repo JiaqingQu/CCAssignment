3f61d7e126b4c3dea01d9edee07637f9cc41bc36
qujiaqing <jiaqing_qu@yeah.net>
2015-09-24 10:44:01 -0400

    update week2 assignment's first commit

--- ch4/Solution06.java
+++ ch4/Solution06.java
@@ -63,7 +63,6 @@ public class Solution06 {
         n8.setlChild(n7);
         n10.setrChild(n11);
 
-        BTreePrinter.printNode(n6);
 
         System.out.format("Successor of %d is %d",n5.val,inOrderSuccessor(n5).val);
     }
@@ -93,98 +92,7 @@ public class Solution06 {
 
     }
 
-    /**
-     * [Reference: This Tree Printer is referred from
-     *  http://stackoverflow.com/questions/4965335/how-to-print-binary-tree-diagram
-     *  for testing
-     * ]
-     * */
-    static class BTreePrinter {
 
-        public static void printNode(BinaryTreeNode root) {
-            int maxLevel = BTreePrinter.maxLevel(root);
-
-            printNodeInternal(Collections.singletonList(root), 1, maxLevel);
-        }
-
-        private static <T extends Comparable<?>> void printNodeInternal(List<BinaryTreeNode> nodes, int level, int maxLevel) {
-            if (nodes.isEmpty() || BTreePrinter.isAllElementsNull(nodes))
-                return;
-
-            int floor = maxLevel - level;
-            int endgeLines = (int) Math.pow(2, (Math.max(floor - 1, 0)));
-            int firstSpaces = (int) Math.pow(2, (floor)) - 1;
-            int betweenSpaces = (int) Math.pow(2, (floor + 1)) - 1;
-
-            BTreePrinter.printWhitespaces(firstSpaces);
-
-            List<BinaryTreeNode> newNodes = new ArrayList<BinaryTreeNode>();
-            for (BinaryTreeNode node : nodes) {
-                if (node != null) {
-                    System.out.print(node.val);
-                    newNodes.add(node.lChild);
-                    newNodes.add(node.rChild);
-                } else {
-                    newNodes.add(null);
-                    newNodes.add(null);
-                    System.out.print(" ");
-                }
-
-                BTreePrinter.printWhitespaces(betweenSpaces);
-            }
-            System.out.println("");
-
-            for (int i = 1; i <= endgeLines; i++) {
-                for (int j = 0; j < nodes.size(); j++) {
-                    BTreePrinter.printWhitespaces(firstSpaces - i);
-                    if (nodes.get(j) == null) {
-                        BTreePrinter.printWhitespaces(endgeLines + endgeLines + i + 1);
-                        continue;
-                    }
-
-                    if (nodes.get(j).lChild != null)
-                        System.out.print("/");
-                    else
-                        BTreePrinter.printWhitespaces(1);
-
-                    BTreePrinter.printWhitespaces(i + i - 1);
-
-                    if (nodes.get(j).lChild != null)
-                        System.out.print("\\");
-                    else
-                        BTreePrinter.printWhitespaces(1);
-
-                    BTreePrinter.printWhitespaces(endgeLines + endgeLines - i);
-                }
-
-                System.out.println("");
-            }
-
-            printNodeInternal(newNodes, level + 1, maxLevel);
-        }
-
-        private static void printWhitespaces(int count) {
-            for (int i = 0; i < count; i++)
-                System.out.print(" ");
-        }
-
-        private static int maxLevel(BinaryTreeNode node) {
-            if (node == null)
-                return 0;
-
-            return Math.max(BTreePrinter.maxLevel(node.lChild), BTreePrinter.maxLevel(node.rChild)) + 1;
-        }
-
-        private static <T> boolean isAllElementsNull(List<T> list) {
-            for (Object object : list) {
-                if (object != null)
-                    return false;
-            }
-
-            return true;
-        }
-
-    }
 
 
 }
